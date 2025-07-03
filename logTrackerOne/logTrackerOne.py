319d5a5c2d3cd02bd36d2b932a9083e27001b4a1
patkag <14110551+patkag@users.noreply.github.com>
2025-07-03 08:44:57 +0300

    added main; a bit of code cleanup

--- logTrackerOne/logTrackerOne.py
+++ logTrackerOne/logTrackerOne.py
@@ -35,36 +35,42 @@ def watch(fn):
             else:
                 time.sleep(1)
 
-fn = logFilePath
-words = ['word']
-isHeaderDisplayed = False
-HeaderFields = {'CALL': 10, 'GRIDSQUARE': 10, 'MODE': 6, 'RST_SENT': 8, 'RST_RCVD': 8, 'QSO_DATE': 9, 'TIME_ON': 7, 'TIME_OFF': 8, 'BAND': 4, 'FREQ': 12, 'MY_GRIDSQUARE': 13, 'TX_PWR': 6}
-#, 'COMMENT': 25}
-separator = 7+ 10+ 4+ 8+ 8+ 9+ 7+ 8+ 4+ 9
+def displayHeader(HeaderFields):
+    row_color = Fore.GREEN + Style.BRIGHT
+    column_widths = HeaderFields
+    separator = 68
+
+    header_line = " | ".join(f"{header:<{column_widths[header]}}" for header in HeaderFields)
+    separator_line = "-".join("-" * separator)
+
+    #print fancy header with separator line
+    print(row_color + header_line)
+    print(row_color + separator_line)
 
 
-try:
-    for line in watch(fn):
-        parsed_data = parse_line(line)
-        if isHeaderDisplayed == False:
+def main():
+    fn = logFilePath
+    words = ['word']
+    isHeaderDisplayed = False
+    HeaderFields = {'CALL': 10, 'GRIDSQUARE': 10, 'MODE': 6, 'RST_SENT': 8, 'RST_RCVD': 8, 'QSO_DATE': 9, 'TIME_ON': 7, 'TIME_OFF': 8, 'BAND': 4, 'FREQ': 12, 'MY_GRIDSQUARE': 13, 'TX_PWR': 6}
+    #, 'COMMENT': 25}
+    column_widths = HeaderFields
 
-            parsed_line = parsed_data.keys()
+    #set the color to bright green so it's easy to read
+    row_color = Fore.GREEN + Style.BRIGHT
 
-            column_widths = HeaderFields
-         
-            header_line = " | ".join(f"{header:<{column_widths[header]}}" for header in HeaderFields)
-            separator_line = "-".join("-" * separator)
+    try:
+        displayHeader(HeaderFields)
+        for line in watch(fn):
+            parsed_data = parse_line(line)
+                        
+            #only display QSOs made today
+            if(parsed_data.get('QSO_DATE', '').startswith(currentdate)):
+                print(row_color + " | ".join(f"{parsed_data.get(vals, ''):<{column_widths[vals]}}" for vals in HeaderFields))
 
-            #print fancy header with separator line
-            print(header_line)
-            print(separator_line)
-            isHeaderDisplayed=True
+    except KeyboardInterrupt:
+        exit
 
-        #set the color to bright green so it's easy to read
-        row_color = Fore.GREEN + Style.BRIGHT
-        #only display QSOs made today
-        if(parsed_data.get('QSO_DATE', '').startswith(currentdate)):
-            print(row_color + " | ".join(f"{parsed_data.get(vals, ''):<{column_widths[vals]}}" for vals in HeaderFields))
+if __name__ == "__main__":
+    main()
 
-except KeyboardInterrupt:
-    exit
